3cad4a5b9828bba278cee513bf6a9cd53e5962f4
wVelpro <88667126+wVelpro@users.noreply.github.com>
2022-08-07 01:16:51 +0800

    完成个人信息栏

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <MyNav></MyNav>
     <!-- <EntryList></EntryList> -->
+    <!-- <AuthorInfo></AuthorInfo> -->
   </div>
 </template>
 
 <script>
 import MyNav from './components/MyNav/MyNav.vue';
 import EntryList from './components/page/EntryList.vue';
+import AuthorInfo from './components/page/AuthorInfo.vue';
 
 export default {
   name: 'App',
   components: {
     MyNav,
-    EntryList
-}
+    EntryList,
+    AuthorInfo
+  }
 }
 </script>
 
