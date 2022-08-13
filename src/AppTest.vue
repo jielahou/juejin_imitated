59340f1f7fbe39d2a1af79de9c30f74f7285aaf3
Orphea <3367436163@qq.com>
2022-08-14 02:18:21 +0800

    '重做了顶部导航栏，相似度99%'

--- src/AppTest.vue
+++ src/AppTest.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <MyTopNav></MyTopNav>
-    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
-import App from './App.vue'
+// import App from './App.vue'
+
+// 测试用
+import App from './AppTest.vue'
 
 Vue.use(ElementUI);
 
