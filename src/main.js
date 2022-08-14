efdb7d094ccfe8bb5ab089f26383e9ab95af69c6
wVelpro <88667126+wVelpro@users.noreply.github.com>
2022-08-15 01:15:10 +0800

    基本完成Markdown文章显示

--- src/main.js
+++ src/main.js
@@ -7,6 +7,18 @@ import router from './router'
 // 测试用
 import App from './AppTest.vue'
 
+//highlight
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css' //样式
+//创建v-highlight全局指令
+Vue.directive('highlight',function (el) {
+  let blocks = el.querySelectorAll('pre code');
+  blocks.forEach((block)=>{
+    hljs.highlightBlock(block)
+  })
+})
+
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
